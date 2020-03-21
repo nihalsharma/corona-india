4ae0553d4dc0e5678443cb11d276607d7a07ec9f
nihalsharma <nihaldps@gmail.com>
2020-03-22 00:53:21 +0530

    drop downs

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def world_stat():
         if d['country'] == 'Total:':
             total = d
             break
-    return flask.render_template('world-stats.html', data=data, total=total)
+    return flask.render_template('world-stats.html', data=data, total=total, open_world="open")
 
 
 @app.route('/india-stats')
@@ -66,14 +66,14 @@ def india_stats():
         if d['country'] == 'India':
             total = d
             break
-    return flask.render_template('india-stats.html', total=total, data=dataset)
+    return flask.render_template('india-stats.html', total=total, data=dataset, open_india="open")
 
 
 @app.route('/india-live')
 def india_live():
     db_conn = DBConnection()
     india_news = db_conn.db_conn.fetch_india_news()
-    return flask.render_template('india-live.html', india_news=india_news)
+    return flask.render_template('india-live.html', india_news=india_news, open_india="open")
 
 
 @app.route('/world-live')
