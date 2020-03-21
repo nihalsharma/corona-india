94d5320bf2de855216cdba6ca3b816ecb0cf4387
nihalsharma <nihaldps@gmail.com>
2020-03-22 04:02:39 +0530

    prod settings

--- settings/prod.py
+++ settings/prod.py
@@ -1,1 +1,7 @@
 MONGODB_HOST = 'localhost'
+CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+CELERY_REDIS_HOST = "localhost"
+CELERY_REDIS_PORT = 6379
+CELERY_REDIS_DB = 0
+BROKER_URL = "redis://localhost:6379/0"
+BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 1500}
